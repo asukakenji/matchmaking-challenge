2ace6026c1e4952388b9d1549f276ec0622250cd
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-17 21:49:54 +0800

    Added 2 default methods

--- src/main/java/org/devnull/matchmaking/PlayerInterface.java
+++ src/main/java/org/devnull/matchmaking/PlayerInterface.java
@@ -28,4 +28,33 @@ public interface PlayerInterface {
      */
     int getLosses();
 
+    /**
+     * Returns the win rate of this player.
+     *
+     * @return the win rate of this player,
+     * which is a floating-pointer number between 0.0 and 1.0 inclusive
+     */
+    // Interview Note: Java Feature - Default Methods
+    default double getWinRate() {
+        // See docs/assumptions_and_ideas.md for explanation
+        // rate = wins / (wins + losses)
+        //     = 1 / [ (wins + losses) / wins ]
+        //     = 1 / [ 1 + (losses / wins) ]
+        final double wins = (double) this.getWins();
+        final double losses = (double) this.getLosses();
+        return 1.0 / (1.0 + losses / wins);
+    }
+
+    /**
+     * Returns the number of battles this player has fought.
+     *
+     * @return the number of battles this player has fought,
+     * which is a non-negative integer
+     */
+    default long getBattlesFought() {
+        final long wins = (long) this.getWins();
+        final long losses = (long) this.getLosses();
+        return wins + losses;
+    }
+
 }
