872cd078a2d49b85dce7b8f128a1cb4f4dfca3f9
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-17 21:32:07 +0800

    Added comments

--- src/main/java/org/devnull/matchmaking/PlayerInterface.java
+++ src/main/java/org/devnull/matchmaking/PlayerInterface.java
@@ -1,11 +1,31 @@
 package org.devnull.matchmaking;
 
+/**
+ * TODO: Write this!
+ */
 public interface PlayerInterface {
 
+    /**
+     * Returns the name of this player.
+     *
+     * @return the name of this player, which is a non-null String
+     */
     String getName();
 
+    /**
+     * Returns the number of battles this player has won.
+     *
+     * @return the number of battles this player has won,
+     * which is a non-negative integer
+     */
     int getWins();
 
+    /**
+     * Returns the number of battles this player has lost.
+     *
+     * @return the number of battles this player has lost,
+     * which is a non-negative integer
+     */
     int getLosses();
 
 }
