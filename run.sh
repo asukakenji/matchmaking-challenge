a3a7bcd335d278098ebcd734a0eb952892d31a73
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-17 22:10:29 +0800

    Added PlayerToBeMatched.java

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 java -cp classes org.devnull.matchmaking.Player
+java -cp classes org.devnull.matchmaking.PlayerToBeMatched
+serialver -classpath classes org.devnull.matchmaking.Player
+serialver -classpath classes org.devnull.matchmaking.Player.NameComparator
+serialver -classpath classes org.devnull.matchmaking.PlayerToBeMatched
