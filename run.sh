dc773eac783ccdeb315b0d80e116942cf1f87e44
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-19 03:46:18 +0800

    Renamed RemotePlayer.java to PlayerFromDatabase.java

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ serialver -classpath classes org.devnull.matchmaking.player.BasicPlayer
 serialver -classpath classes org.devnull.matchmaking.player.BasicPlayer.NameComparator
 serialver -classpath classes org.devnull.matchmaking.player.PlayerToBeMatched
 serialver -classpath classes org.devnull.matchmaking.player.EquippedPlayer
-serialver -classpath classes org.devnull.matchmaking.player.RemotePlayer
+serialver -classpath classes org.devnull.matchmaking.player.PlayerFromDatabase
 serialver -classpath classes org.devnull.matchmaking.Match
 serialver -classpath classes org.devnull.matchmaking.statistics.BasicMatchStatistics
