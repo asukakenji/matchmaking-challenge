94c032717ab78a61077eab54fb0c97c5db4d3f40
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-19 07:23:35 +0800

    Updated MatchStatistics.java and BasicMatchStatistics.java; Added map based properties

--- src/main/java/org/devnull/matchmaking/MatchStatistics.java
+++ src/main/java/org/devnull/matchmaking/MatchStatistics.java
@@ -1,8 +1,26 @@
 package org.devnull.matchmaking;
 
+/**
+ * TODO: Write this!
+ */
 @FunctionalInterface
 public interface MatchStatistics {
 
     Object getStatistics(String key);
+
+    public static final String TEAM1_WIN_RATE_MIN = "team1.winRate.min";
+    public static final String TEAM1_WIN_RATE_MAX = "team1.winRate.max";
+    public static final String TEAM1_BATTLE_EXPERIENCE_MIN = "team1.battleExperience.min";
+    public static final String TEAM1_BATTLE_EXPERIENCE_MAX = "team1.battleExperience.max";
+
+    public static final String TEAM2_WIN_RATE_MIN = "team2.winRate.min";
+    public static final String TEAM2_WIN_RATE_MAX = "team2.winRate.max";
+    public static final String TEAM2_BATTLE_EXPERIENCE_MIN = "team2.battleExperience.min";
+    public static final String TEAM2_BATTLE_EXPERIENCE_MAX = "team2.battleExperience.max";
+
+    public static final String MATCH_WIN_RATE_MIN = "match.winRate.min";
+    public static final String MATCH_WIN_RATE_MAX = "match.winRate.max";
+    public static final String MATCH_BATTLE_EXPERIENCE_MIN = "match.battleExperience.min";
+    public static final String MATCH_BATTLE_EXPERIENCE_MAX = "match.battleExperience.max";
 
 }
