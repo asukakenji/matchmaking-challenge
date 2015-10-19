652ee74183a5c99643d5cff6ad41abe4292328ba
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-19 16:30:15 +0800

    Updated pom.xml; Removed make.sh (completely replaced by pom.xml)

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p classes
-javac -Xlint:unchecked -Xdiags:verbose -sourcepath src/main/java -d classes src/main/java/TestMain.java src/main/java/org/devnull/matchmaking/*.java src/main/java/org/devnull/matchmaking/*/*.java
