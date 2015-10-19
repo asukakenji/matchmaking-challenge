4fb4f432e4834a50a51dd511223065e6211ea14a
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-19 16:34:29 +0800

    Updated .gitignore and run.sh to follow the Maven directories

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-java -cp classes TestMain
-java -cp classes org.devnull.matchmaking.Main
-#serialver -classpath classes org.devnull.matchmaking.player.BasicPlayer
-#serialver -classpath classes org.devnull.matchmaking.player.BasicPlayer.NameComparator
-#serialver -classpath classes org.devnull.matchmaking.player.EquippedPlayer
-#serialver -classpath classes org.devnull.matchmaking.player.PlayerFromDatabase
-#serialver -classpath classes org.devnull.matchmaking.player.PlayerWithNameBasedEquality
-#serialver -classpath classes org.devnull.matchmaking.Match
-#serialver -classpath classes org.devnull.matchmaking.statistics.BasicMatchStatistics
+java -cp target/classes TestMain
+java -cp target/classes org.devnull.matchmaking.Main
+#serialver -classpath target/classes org.devnull.matchmaking.player.BasicPlayer
+#serialver -classpath target/classes org.devnull.matchmaking.player.BasicPlayer.NameComparator
+#serialver -classpath target/classes org.devnull.matchmaking.player.EquippedPlayer
+#serialver -classpath target/classes org.devnull.matchmaking.player.PlayerFromDatabase
+#serialver -classpath target/classes org.devnull.matchmaking.player.PlayerWithNameBasedEquality
+#serialver -classpath target/classes org.devnull.matchmaking.Match
+#serialver -classpath target/classes org.devnull.matchmaking.statistics.BasicMatchStatistics
