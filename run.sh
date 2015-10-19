8419041d62eac0ca21bb140f1a93cbd99890e688
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-19 18:15:19 +0800

    Added exec-maven-plugin; Got rid of running Main in run.sh - use mvn exec:java now

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 java -cp target/classes TestMain
-java -cp target/classes org.devnull.matchmaking.Main
+#java -cp target/classes org.devnull.matchmaking.Main
 #serialver -classpath target/classes org.devnull.matchmaking.player.BasicPlayer
 #serialver -classpath target/classes org.devnull.matchmaking.player.BasicPlayer.NameComparator
 #serialver -classpath target/classes org.devnull.matchmaking.player.EquippedPlayer
